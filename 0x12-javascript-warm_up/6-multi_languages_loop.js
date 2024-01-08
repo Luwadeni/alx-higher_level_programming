597ed2d07da98fe758392d85754ff1fd59bc3d6c
ifeoluwaoludamilare@gmail.com <ifeoluwaoludamilare@gmail.com>
2024-01-08 13:05:20 -0800

    javascript solution

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+/*
+ * script that prints 3 lines: (like 1-multi_languages.js) but by using an array of string and a loop
+ */
+for (const str of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(str);
+}
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,1 @@
+Solution to Javascript Warm up
