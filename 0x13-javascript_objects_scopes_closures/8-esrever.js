b0aec1e9536c9819e14961bff49b4c0d1fc38a1a
ifeoluwaoludamilare@gmail.com <ifeoluwaoludamilare@gmail.com>
2024-01-10 08:42:46 -0800

    8&9 js file

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/**
+ * function that returns the reversed version of a list
+ * @param {*} list
+ *
+ * Run `./maintest/8-main.js` to test
+ */
+exports.esrever = function (list) {
+  const reversedArray = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedArray.push(list[i]);
+  }
+  return reversedArray;
+};
