beb2d81ed3dafec411ddc8028fcbebf4b1c35898
ifeoluwaoludamilare@gmail.com <ifeoluwaoludamilare@gmail.com>
2024-01-26 16:50:16 -0800

    Github - Listing recent commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A Python script that list 10 commits (from the most recent to oldest) of
+the repository
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    commits = requests.get(url).json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
